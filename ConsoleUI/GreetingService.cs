d61fb5d7b27b502c26749e1e50264dad22200610
Ivan Pesenti <ivan.pesenti@abenergie.it>
2021-02-22 15:56:42 +0100

    commit

---
+++ ConsoleUI/GreetingService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+// DI, Serilog, Settings
+
+namespace ConsoleUI
+{
+    public class GreetingService : IGreetingService
+    {
+        private readonly ILogger<GreetingService> _log;
+        private readonly IConfiguration _config;
+
+        public GreetingService(ILogger<GreetingService> log, IConfiguration config)
+        {
+            _log = log;
+            _config = config;
+        }
+
+        public void Run()
+        {
+            for (int i = 0; i < _config.GetValue<int>("LoopTimes"); i++)
+            {
+                _log.LogError("Run number {runNumber}", i);
+            }
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
+
+// DI, Serilog, Settings
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Build())
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+
+            Log.Logger.Information("Application Starting");
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddTransient<IGreetingService, GreetingService>();
+                })
+                .UseSerilog()
+                .Build();
+
+            var svc = ActivatorUtilities.CreateInstance<IGreetingService>(host.Services);
+            svc.Run();
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                .AddEnvironmentVariables();
+        }
+    }
+}
